7f292bae5332901dae03e0fed9484662603840f0
slambuilder <slambuilder@users.noreply.github.com>
2018-08-04 16:11:28 -0700

    refactor ADC-to-DAC transfer example to use global structures.

--- datacap-mcu/src/AppData.h
+++ datacap-mcu/src/AppData.h
@@ -6,6 +6,9 @@ typedef struct TAppData
     TaskHandle_t hTaskRedLedControl;
 	QueueHandle_t hTickQueue;
 	TimerHandle_t hTimer;
+    struct dac_module dacModule;
+    struct adc_module adcModule;
+    struct dma_resource dmaResource;
 } AppData;
 
 #endif
--- datacap-mcu/src/main.c
+++ datacap-mcu/src/main.c
@@ -12,55 +12,54 @@ void taskRedLedControl(void *pvParameters);
 
 // ************************************************************************************************************************************************************
 
-void configure_dma_example(void);
+void configure_dma_adcToDac(struct adc_module *pAdcModule, struct dac_module *pDacModule, DmacDescriptor *pDmacDescriptorAdc);
 
 void configure_dma_resource(struct dma_resource *resource);
-void setup_transfer_descriptor(DmacDescriptor *descriptor);
+void setup_transfer_descriptor(DmacDescriptor *descriptor, struct adc_module *pAdcModule, struct dac_module *pDacModule);
 
-void configure_adc(void);
-void configure_dac(void);
-void configure_dac_channel(void);
+void configure_adc(struct adc_module *pAdcModule);
+void configure_dac(struct dac_module *pDacModule, const enum dac_channel dacChannel);
 
-struct dac_module dac_instance;
-struct adc_module adc_instance;
 struct dma_resource example_resource;
 
 COMPILER_ALIGNED(16)
-DmacDescriptor example_descriptor SECTION_DMAC_DESCRIPTOR;
+DmacDescriptor g_dmacDescriptorAdc SECTION_DMAC_DESCRIPTOR;
 
-void configure_adc(void)
+void configure_adc(struct adc_module *pAdcModule)
 {
-	struct adc_config config_adc;
-	adc_get_config_defaults(&config_adc);
+	struct adc_config configAdc;
+	adc_get_config_defaults(&configAdc);
 
-	config_adc.gain_factor     = ADC_GAIN_FACTOR_2X;
-	config_adc.resolution      = ADC_RESOLUTION_8BIT;
-	config_adc.clock_prescaler = ADC_CLOCK_PRESCALER_DIV16;
-	config_adc.reference       = ADC_REFERENCE_INTVCC1;
-	config_adc.positive_input  = CONF_BOARD_ANALOG_CH1;
-	config_adc.freerunning     = true;
-	config_adc.left_adjust     = false;
+	configAdc.gain_factor     = ADC_GAIN_FACTOR_2X;
+	configAdc.resolution      = ADC_RESOLUTION_8BIT;
+	configAdc.clock_prescaler = ADC_CLOCK_PRESCALER_DIV16;
+	configAdc.reference       = ADC_REFERENCE_INTVCC1;
+	configAdc.positive_input  = CONF_BOARD_ANALOG_CH1;
+    configAdc.pin_scan.inputs_to_scan = 4;
+	configAdc.freerunning     = true;
+	configAdc.left_adjust     = false;
 
-	adc_init(&adc_instance, ADC, &config_adc);
-	adc_enable(&adc_instance);
+	adc_init(pAdcModule, ADC, &configAdc);
+	adc_enable(pAdcModule);
 }
 
-void configure_dac(void)
+void configure_dac(struct dac_module *pDacModule, const enum dac_channel dacChannel)
 {
-	struct dac_config config_dac;
-	dac_get_config_defaults(&config_dac);
+	struct dac_config configDac;
+	dac_get_config_defaults(&configDac);
 
-	config_dac.reference = DAC_REFERENCE_AVCC;
-	dac_init(&dac_instance, DAC, &config_dac);
-}
+	configDac.reference = DAC_REFERENCE_AVCC;
+	dac_init(pDacModule, DAC, &configDac);
 
-void configure_dac_channel(void)
-{
+    // set DAC channel
 	struct dac_chan_config config_dac_chan;
-
 	dac_chan_get_config_defaults(&config_dac_chan);
-	dac_chan_set_config(&dac_instance, DAC_CHANNEL_0, &config_dac_chan);
-	dac_chan_enable(&dac_instance, DAC_CHANNEL_0);
+
+	dac_chan_set_config(pDacModule, dacChannel, &config_dac_chan);
+	dac_chan_enable(pDacModule, dacChannel);
+
+    // enable DAC
+	dac_enable(pDacModule);
 }
 
 void configure_dma_resource(struct dma_resource *resource)
@@ -74,7 +73,7 @@ void configure_dma_resource(struct dma_resource *resource)
 	dma_allocate(resource, &config);
 }
 
-void setup_transfer_descriptor(DmacDescriptor *descriptor)
+void setup_transfer_descriptor(DmacDescriptor *descriptor, struct adc_module *pAdcModule, struct dac_module *pDacModule)
 {
 	struct dma_descriptor_config descriptor_config;
 
@@ -84,25 +83,23 @@ void setup_transfer_descriptor(DmacDescriptor *descriptor)
 	descriptor_config.dst_increment_enable = false;
 	descriptor_config.src_increment_enable = false;
 	descriptor_config.block_transfer_count = 1000;
-	descriptor_config.source_address = (uint32_t)(&adc_instance.hw->RESULT.reg);
-	descriptor_config.destination_address = (uint32_t)(&dac_instance.hw->DATA.reg);
+	descriptor_config.source_address = (uint32_t)(&pAdcModule->hw->RESULT.reg);
+	descriptor_config.destination_address = (uint32_t)(&pDacModule->hw->DATA.reg);
 	descriptor_config.next_descriptor_address = (uint32_t)descriptor;
 
 	dma_descriptor_create(descriptor, &descriptor_config);
 }
 
-void configure_dma_example(void)
+void configure_dma_adcToDac(struct adc_module *pAdcModule, struct dac_module *pDacModule, DmacDescriptor *pDmacDescriptorAdc)
 {
 	system_init();
 
-	configure_adc();
-	configure_dac();
-	configure_dac_channel();
-	dac_enable(&dac_instance);
+	configure_adc(pAdcModule);
+	configure_dac(pDacModule, DAC_CHANNEL_0);
 	configure_dma_resource(&example_resource);
-	setup_transfer_descriptor(&example_descriptor);
-	dma_add_descriptor(&example_resource, &example_descriptor);
-	adc_start_conversion(&adc_instance);
+	setup_transfer_descriptor(pDmacDescriptorAdc, pAdcModule, pDacModule);
+	dma_add_descriptor(&example_resource, pDmacDescriptorAdc);
+	adc_start_conversion(pAdcModule);
 	dma_start_transfer_job(&example_resource);
 }
 
@@ -139,7 +136,7 @@ int main (void)
 	xTimerStart(g_appData.hTimer, 0);
 
     // start DMA transfer
-    configure_dma_example();
+    configure_dma_adcToDac(&g_appData.adcModule, &g_appData.dacModule, &g_dmacDescriptorAdc);
 
 	// Start the scheduler. Normally, this function never returns.
 	vTaskStartScheduler();
